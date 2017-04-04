8238784f11391f8f9d7fd3bf2615cdc78070a076
NualartJavier <xavier.nualart.14@gmail.com>
2017-04-03 21:22:20 -0400

    instanceof

---
+++ Ayudantia/tema2/Naranja.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudantia.tema2;
+
+/**
+ *
+ * @author juan
+ */
+public class Naranja extends Fruta {
+
+    public Naranja(String nombre) {
+        super(nombre);
+    }
+
+    
+    
+}
