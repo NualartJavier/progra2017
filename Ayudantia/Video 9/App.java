1dd50d8a7f17fb730e2663e31fc74d4c0757bb6f
NualartJavier <xavier.nualart.14@gmail.com>
2017-04-11 10:31:54 -0400

    Capacidad inicial

---
+++ Ayudantia/Video 9/App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudantia.tema9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author juan
+ */
+public class App {
+    public static void main(String []args){
+    List<String> lista = new ArrayList();
+    System.out.println(lista.toString());
+    }
+}
