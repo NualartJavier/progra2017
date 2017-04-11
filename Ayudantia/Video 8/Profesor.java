765b9be927252f1649153e3cac11be0900d6b07a
NualartJavier <xavier.nualart.14@gmail.com>
2017-04-11 09:19:19 -0400

    Generios 3

---
+++ Ayudantia/Video 8/Profesor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudantia.tema6;
+
+/**
+ *
+ * @author juan
+ */
+public class Profesor extends Persona {
+
+    public Profesor(String nombre) {
+        super(nombre);
+    }
+    
+}
