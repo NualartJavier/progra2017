a0c2e95cb0c9c393ef9e57f307cbecf0233f9c67
NualartJavier <xavier.nualart.14@gmail.com>
2017-04-11 09:05:17 -0400

    Genericos 2

---
+++ Ayudantia/Video 7/Alumno.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudantia.tema6;
+
+/**
+ *
+ * @author juan
+ */
+public class Alumno extends Persona{
+
+    public Alumno(String nombre) {
+        super(nombre);
+    }
+    
+
+    
+    
+    
+}
