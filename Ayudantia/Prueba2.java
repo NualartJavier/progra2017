a6c9056c2e74b5fab6d03df33d1a78530670bcdf
NualartJavier <xavier.nualart.14@gmail.com>
2017-04-11 09:07:16 -0400

    gg

--- Ayudantia/Prueba2.java
+++
@@ -1,7 +0,0 @@
-//Ingresar 3 nombres y saludar al primero y al tercero
-public class Prueba2 {
-	public static void main(String... args){
-		System.out.println("Hola "+args[0]+" y " + args[2]);
-		
-	}
-}
--- Ayudantia/Prueba3.java
+++
@@ -1,7 +0,0 @@
-public class Prueba3 {
-	public static void main(String... args){
-	System.out.println(args[0].charAt(0));
-	System.out.println(args[0].charAt(args[0].length()-1));
-		
-	}
-}
